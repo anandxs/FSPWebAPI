a38dd0f40ebc753e688dddc95f253e0fe75a5c60
Anand S <anandsatheeshk@gmail.com>
2023-11-28 10:52:14 +0530

    Role not found exception

---
+++ Entities/Exceptions/RoleNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.Exceptions
+{
+    public sealed class RoleNotFoundException : NotFoundException
+    {
+        public RoleNotFoundException(Guid id) : base($"Role with id : {id} not foound.")
+        {
+        }
+    }
+}
