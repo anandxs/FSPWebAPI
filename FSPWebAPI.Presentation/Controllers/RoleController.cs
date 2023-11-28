b87b703520ecec2dee85e88d54b91aeb3529af11
Anand S <anandsatheeshk@gmail.com>
2023-11-28 07:16:59 +0530

    Role controller created

---
+++ FSPWebAPI.Presentation/Controllers/RoleController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+
+namespace FSPWebAPI.Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/users/{userId}/projects/{projectId}/roles")]
+    [Authorize]
+    public class RoleController : ControllerBase
+    {
+        private readonly IServiceManager _service;
+
+        public RoleController(IServiceManager service)
+        {
+            _service = service;
+        }
+    }
+}
