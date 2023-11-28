b3a377365f6476566dde595ec8b8515916c9f7f4
Anand S <anandsatheeshk@gmail.com>
2023-11-28 10:54:34 +0530

    Invite user serivce declaration

---
+++ Shared/DataTransferObjects/MemberForCreationDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.DataTransferObjects
+{
+    public record MemberForCreationDto
+    {
+        [Required(ErrorMessage = "Email is a required field.")]
+        public string Email { get; init; }
+        [Required(ErrorMessage = "RoleId is a required field.")]
+        public Guid RoleId { get; init; }
+    }
+}
