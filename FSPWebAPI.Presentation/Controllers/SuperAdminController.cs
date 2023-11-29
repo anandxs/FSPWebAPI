79ba6d3989adda0ad72237c674d6795f30f9bb59
Anand S <anandsatheeshk@gmail.com>
2023-11-29 16:53:25 +0530

    Superadmin controller created with roles endpoint

---
+++ FSPWebAPI.Presentation/Controllers/SuperAdminController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+
+namespace FSPWebAPI.Presentation.Controllers
+{
+    [Route("api/superadmin")]
+    [ApiController]
+    public class SuperAdminController : ControllerBase
+    {
+        private readonly IServiceManager _service;
+
+        public SuperAdminController(IServiceManager service)
+        {
+            _service = service;
+        }
+
+        #region DEFAULT PROJECT ROLE MANAGEMENT
+
+        [HttpGet("roles")]
+        public IActionResult GetProjectRoles()
+        {
+            return Ok();
+        }
+
+        [HttpPost("roles")]
+        public IActionResult CreateProjectRole()
+        {
+            return NoContent();
+        }
+
+        [HttpPut("roles/{roleId:guid}")]
+        public IActionResult UpdateProjectRole(Guid roleId)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete("roles/{roleId:guid}")]
+        public IActionResult DeleteProjectRole(Guid roleId)
+        {
+            return NoContent();
+        }
+
+        #endregion
+    }
+}
