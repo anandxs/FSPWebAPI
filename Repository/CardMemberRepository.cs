d9bac56ef0a02edfc4b1fed1cd5117b3a02eba4e
Anand S <anandsatheeshk@gmail.com>
2023-12-09 09:46:25 +0530

    CardMember repository registration

---
+++ Repository/CardMemberRepository.cs
@@ -0,0 +1,12 @@
+﻿using Contracts;
+using Entities.Models;
+
+namespace Repository
+{
+    public class CardMemberRepository : RepositoryBase<CardMember>, ICardMemberRepository
+    {
+        public CardMemberRepository(RepositoryContext repositoryContext) : base(repositoryContext)
+        {
+        }
+    }
+}
