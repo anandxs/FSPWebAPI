bbfbed8a2df8be3f3f604bf1b3b0b59ebd1970fd
Anand S <anandsatheeshk@gmail.com>
2023-12-12 12:18:49 +0530

    Authentication service exceptions

---
+++ Entities/Exceptions/BlockedUserForbiddenRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.Exceptions
+{
+    public sealed class BlockedUserForbiddenRequest : ForbiddenRequestException
+    {
+        public BlockedUserForbiddenRequest() : base($"You have been blocked from the application.")
+        {
+        }
+    }
+}
