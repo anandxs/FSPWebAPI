df1ac08a2a112391357ab8cb4de354d076a4ccea
Anand S <anandsatheeshk@gmail.com>
2023-12-12 12:32:22 +0530

    Cardmember service exceptions

---
+++ Entities/Exceptions/MemberAlreadyAssignedToCardBadRequestException.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.Exceptions
+{
+    public sealed class MemberAlreadyAssignedToCardBadRequestException : BadRequestException
+    {
+        public MemberAlreadyAssignedToCardBadRequestException() : base($"Member is already assigned to this card.")
+        {
+        }
+    }
+}
---
+++ Entities/Exceptions/NotAProjectMemberBadRequestException.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.Exceptions
+{
+    public sealed class NotAProjectMemberBadRequestException : BadRequestException
+    {
+        public NotAProjectMemberBadRequestException(string id) : base($"User with id : {id} is not a member of project.")
+        {
+        }
+    }
+}
