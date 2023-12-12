51f5bdae4a0455ec7101aa5de186bef56485d8f7
Anand S <anandsatheeshk@gmail.com>
2023-12-12 13:15:19 +0530

    Member service excpetions

---
+++ Entities/Exceptions/NotEnoughAdminsBadRequestException.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.Exceptions
+{
+    public sealed class NotEnoughAdminsBadRequestException : BadRequestException
+    {
+        public NotEnoughAdminsBadRequestException() : base($"Project needs to have atleast one admin.")
+        {
+        }
+    }
+}
