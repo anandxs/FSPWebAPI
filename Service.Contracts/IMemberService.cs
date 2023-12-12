dee9fd3e97a64dc7df6c322435e18b9eff3ba997
Anand S <anandsatheeshk@gmail.com>
2023-12-12 17:22:51 +0530

    Member service get project member method

--- Service.Contracts/IMemberService.cs
+++ Service.Contracts/IMemberService.cs
@@ -5,6 +5,7 @@ namespace Service.Contracts
     public interface IMemberService
     {
         Task<IEnumerable<ProjectMemberDto>> GetAllProjectMembersAsync(Guid projectId, string requesterId, bool trackChanges);
+        Task<ProjectMemberDto> GetProjectMemberAsync(Guid projectId, string memberId,string requesterId, bool trackChanges);
         Task AddMemberAsync(Guid projectId, string requesterId, MemberForCreationDto memberDto, bool trackChanges);
         Task ChangeMemberRoleAsync(Guid projectId, string requesterId, MemberForUpdateDto memberDto, bool trackChanges);
         Task RemoveMemberAsync(Guid projectId, string memberId, string requesterId, bool trackChanges);
