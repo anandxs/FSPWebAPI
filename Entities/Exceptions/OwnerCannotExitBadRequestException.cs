f6c83de4bca91e6ab0d6176ad67a0e32c90b7bdf
Anand S <anandsatheeshk@gmail.com>
2023-12-13 13:49:59 +0530

    Rename owner exception

--- Entities/Exceptions/OwnerCannotExitBadRequestException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Entities.Exceptions
-{
-    public sealed class OwnerCannotExitBadRequestException : BadRequestException
-    {
-        public OwnerCannotExitBadRequestException() : base($"Owner cannot exit project.")
-        {
-        }
-    }
-}
