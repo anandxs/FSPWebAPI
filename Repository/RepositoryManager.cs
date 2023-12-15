2df5492532d782c62cd74cbb501cff271f8eae22
Anand S <anandsatheeshk@gmail.com>
2023-12-15 15:46:53 +0530

    Tags repository registration

--- Repository/RepositoryManager.cs
+++ Repository/RepositoryManager.cs
@@ -9,6 +9,7 @@ namespace Repository
         private readonly Lazy<IRoleRepository> _roleRepository;
         private readonly Lazy<IStageRepository> _stageRepository;
         private readonly Lazy<ITaskTypeRepository> _taskTypeRepository;
+        private readonly Lazy<ITagRepository> _tagRepository;
         private readonly Lazy<IProjectMemberRepository> _projectMemberRepository;
         private readonly Lazy<ICardRepository> _cardRepository;
         private readonly Lazy<ICardMemberRepository> _cardMemberRepository;
@@ -21,6 +22,7 @@ namespace Repository
             _roleRepository = new Lazy<IRoleRepository>(() => new RoleRepository(repositoryContext));
             _stageRepository = new Lazy<IStageRepository>(() => new StageRepository(repositoryContext));
             _taskTypeRepository = new Lazy<ITaskTypeRepository>(() => new TaskTypeRepository(repositoryContext));
+            _tagRepository = new Lazy<ITagRepository>>(() => new TagRepository(repositoryContext));
             _projectMemberRepository = new Lazy<IProjectMemberRepository>(() => new ProjectMemberRepository(repositoryContext));
             _cardRepository = new Lazy<ICardRepository>(() => new CardRepository(repositoryContext));
             _cardMemberRepository = new Lazy<ICardMemberRepository>(() => new CardMemberRepository(repositoryContext));
@@ -31,6 +33,7 @@ namespace Repository
         public IRoleRepository RoleRepository => _roleRepository.Value;
         public IStageRepository StageRepository => _stageRepository.Value;
         public ITaskTypeRepository TaskTypeRepository => _taskTypeRepository.Value;
+        public ITagRepository TagRepository => _tagRepository.Value;
         public IProjectMemberRepository ProjectMemberRepository => _projectMemberRepository.Value;
         public ICardRepository CardRepository => _cardRepository.Value;
         public ICardMemberRepository CardMemberRepository => _cardMemberRepository.Value;
---
+++ Repository/TagRepository.cs
@@ -0,0 +1,12 @@
+﻿using Contracts;
+using Entities.Models;
+
+namespace Repository
+{
+    public class TagRepository : RepositoryBase<Tag>, ITagRepository
+    {
+        public TagRepository(RepositoryContext repositoryContext) : base(repositoryContext)
+        {
+        }
+    }
+}
