83238d835c62fe564f8b239179184918da35a843
Anand S <anandsatheeshk@gmail.com>
2023-12-15 11:43:10 +0530

    Task type not found exception

---
+++ Entities/Exceptions/TaskTypeNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.Exceptions
+{
+    public sealed class TaskTypeNotFoundException : NotFoundException
+    {
+        public TaskTypeNotFoundException(Guid id) : base($"Task type of id : {id} is not found.")
+        {
+        }
+    }
+}
