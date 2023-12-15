2dfcd8a0dcde70066e76f74b6a235f8d9badb6b4
Anand S <anandsatheeshk@gmail.com>
2023-12-15 10:47:51 +0530

    TaskType service registration

---
+++ Service/TaskTypeService.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Contracts;
+using Microsoft.AspNetCore.Http;
+using Service.Contracts;
+
+namespace Service
+{
+    public class TaskTypeService : ITaskTypeService
+    {
+        private readonly IRepositoryManager _repositoryManager;
+        private readonly ILoggerManager _loggerManager;
+        private readonly IMapper _mapper;
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public TaskTypeService(IRepositoryManager repositoryManager, ILoggerManager loggerManager, IMapper mapper, IHttpContextAccessor contextAccessor)
+        {
+            _repositoryManager = repositoryManager;
+            _loggerManager = loggerManager;
+            _mapper = mapper;
+            _contextAccessor = contextAccessor;
+        }
+    }
+}
