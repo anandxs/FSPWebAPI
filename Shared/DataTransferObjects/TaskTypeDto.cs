2aac0d21055af49b3b395f5be69979d77d398b6f
Anand S <anandsatheeshk@gmail.com>
2023-12-15 11:03:08 +0530

    TaskType Dtos

---
+++ Shared/DataTransferObjects/TaskTypeDto.cs
@@ -0,0 +1,4 @@
+﻿namespace Shared.DataTransferObjects
+{
+    public record TaskTypeDto(Guid TypeId, string Name, bool IsActive);
+}
---
+++ Shared/DataTransferObjects/TaskTypeForUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.DataTransferObjects
+{
+    public record TaskTypeForUpdateDto
+    {
+        [Required(ErrorMessage = "Task type name is a required field.")]
+        public string? Name { get; set; }
+    }
+}
