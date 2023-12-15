230735a14d1deb68e80f0e35e9f6d1e30fba47cf
Anand S <anandsatheeshk@gmail.com>
2023-12-15 14:27:24 +0530

    Tags table created in database

---
+++ Entities/Models/Tag.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models
+{
+    public class Tag
+    {
+        public Guid TagId { get; set; }
+        [Required(ErrorMessage = "Tag name is a required field.")]
+        [MaxLength(256, ErrorMessage = "Maximum length for tag name is 256 characters")]
+        public string? Name { get; set; }
+        [Required(ErrorMessage = "Color is a required field.")]
+        [MaxLength(7, ErrorMessage = "Maximum length for colour is 7 characters")]
+        public string? Colour { get; set; }
+        [ForeignKey(nameof(Project))]
+        public Guid ProjectId { get; set; }
+        public Project Project { get; set; }
+
+    }
+}
---
+++ FSPWebAPI/Migrations/20231215085300_AddTagsTable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FSPWebAPI.Migrations
+{
+    public partial class AddTagsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    TagId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    Colour = table.Column<string>(type: "nvarchar(7)", maxLength: 7, nullable: false),
+                    ProjectId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.TagId);
+                    table.ForeignKey(
+                        name: "FK_Tags_Projects_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Projects",
+                        principalColumn: "ProjectId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tags_ProjectId",
+                table: "Tags",
+                column: "ProjectId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tags");
+        }
+    }
+}
