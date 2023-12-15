4cbc16f54408ef6a9faa52aa632f34e9cc0be1e2
Anand S <anandsatheeshk@gmail.com>
2023-12-15 16:02:39 +0530

    Tags service registration

---
+++ Service/TagService.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Contracts;
+using Microsoft.AspNetCore.Http;
+using Service.Contracts;
+
+namespace Service
+{
+    public class TagService : ITagService
+    {
+        private readonly IRepositoryManager _repositoryManager;
+        private readonly ILoggerManager _logger;
+        private readonly IMapper _mapper;
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public TagService(IRepositoryManager repositoryManager, ILoggerManager logger, IMapper mapper, IHttpContextAccessor contextAccessor)
+        {
+            _repositoryManager = repositoryManager;
+            _logger = logger;
+            _mapper = mapper;
+            _contextAccessor = contextAccessor;
+        }
+    }
+}
