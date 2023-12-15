4cfd28d84ce2f91b064468bdda55db6824c62824
Anand S <anandsatheeshk@gmail.com>
2023-12-15 16:35:09 +0530

    Tag not found exception

---
+++ Entities/Exceptions/TagNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.Exceptions
+{
+    public sealed class TagNotFoundException : NotFoundException
+    {
+        public TagNotFoundException(Guid tagId) : base($"Tag with id : {tagId} not found.")
+        {
+        }
+    }
+}
