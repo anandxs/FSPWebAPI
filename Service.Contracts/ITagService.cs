77faf36c9706514c5c758a2b48f6c48a7ffc25ec
Anand S <anandsatheeshk@gmail.com>
2023-12-15 18:28:11 +0530

    Tag service methods

--- Service.Contracts/ITagService.cs
+++ Service.Contracts/ITagService.cs
@@ -1,6 +1,13 @@
-﻿namespace Service.Contracts
+﻿using Shared.DataTransferObjects;
+
+namespace Service.Contracts
 {
     public interface ITagService
     {
+        Task<IEnumerable<TagDto>> GetAllTagsForProjectAsync(Guid projectId, bool trackChanges);
+        Task<TagDto> GetTagByIdAsync(Guid tagId, bool trackChanges);
+        Task<TagDto> CreateTagAsync(Guid projectId, TagForCreationDto tagForCreationDto, bool trackChanges);
+        Task UpdateTagAsync(Guid tagId, TagForUpdateDto tagForUpdateDto, bool trackChanges);
+        Task DeleteTagAsync(Guid tagId, bool trackChanges);
     }
 }
--- Service/TagService.cs
+++ Service/TagService.cs
@@ -1,7 +1,13 @@
 ﻿using AutoMapper;
 using Contracts;
+using Entities.Exceptions;
+using Entities.Models;
 using Microsoft.AspNetCore.Http;
 using Service.Contracts;
+using Shared;
+using Shared.DataTransferObjects;
+using System;
+using System.Security.Claims;
 
 namespace Service
 {
@@ -18,6 +24,139 @@ namespace Service
             _logger = logger;
             _mapper = mapper;
             _contextAccessor = contextAccessor;
+        }
+
+        public async Task<IEnumerable<TagDto>> GetAllTagsForProjectAsync(Guid projectId, bool trackChanges)
+        {
+            var requesterId = GetRequesterId();
+
+            var requester = await _repositoryManager.ProjectMemberRepository.GetProjectMemberAsync(projectId, requesterId, false);
+
+            if (requester is null)
+            {
+                throw new NotAProjectMemberForbiddenRequestException();
+            }
+
+            var tags = await _repositoryManager.TagRepository.GetAllProjectTagsAsync(projectId, trackChanges);
+
+            var tagsDto = _mapper.Map<IEnumerable<TagDto>>(tags);
+
+            return tagsDto;
+        }
+
+        public async Task<TagDto> GetTagByIdAsync(Guid tagId, bool trackChanges)
+        {
+            var requesterId = GetRequesterId();
+
+            var tag = await _repositoryManager.TagRepository.GetTagByIdAsync(tagId, trackChanges);
+
+            if (tag == null)
+            {
+                throw new TagNotFoundException(tagId);
+            }
+
+            var requester = await _repositoryManager.ProjectMemberRepository.GetProjectMemberAsync(tag.ProjectId, requesterId, false);
+
+            if (requester is null)
+            {
+                throw new NotAProjectMemberForbiddenRequestException();
+            }
+
+            var tagDto = _mapper.Map<TagDto>(tag);
+            
+            return tagDto;
+        }
+
+        public async Task<TagDto> CreateTagAsync(Guid projectId, TagForCreationDto tagForCreationDto, bool trackChanges)
+        {
+            var requesterId = GetRequesterId();
+
+            var requester = await _repositoryManager.ProjectMemberRepository.GetProjectMemberAsync(projectId, requesterId, false);
+
+            if (requester is null)
+            {
+                throw new NotAProjectMemberForbiddenRequestException();
+            }
+            else if (requester.Role.Name != Constants.PROJECT_ROLE_ADMIN)
+            {
+                throw new IncorrectRoleForbiddenRequestException();
+            }
+
+            var project = requester.Project;
+
+            if (project is null)
+            {
+                throw new ProjectNotFoundException(projectId);
+            }
+
+            var tag = _mapper.Map<Tag>(tagForCreationDto);
+
+            _repositoryManager.TagRepository.CreateTag(projectId, tag);
+            await _repositoryManager.SaveAsync();
+
+            var tagDto = _mapper.Map<TagDto>(tag);
+
+            return tagDto;
+        }
+
+        public async Task UpdateTagAsync(Guid tagId, TagForUpdateDto tagForUpdateDto, bool trackChanges)
+        {
+            var requesterId = GetRequesterId();
+
+            var tag = await _repositoryManager.TagRepository.GetTagByIdAsync(tagId, trackChanges);
+
+            if (tag == null)
+            {
+                throw new TaskTypeNotFoundException(tagId);
+            }
+
+            var requester = await _repositoryManager.ProjectMemberRepository.GetProjectMemberAsync(tag.ProjectId, requesterId, false);
+
+            if (requester is null)
+            {
+                throw new NotAProjectMemberForbiddenRequestException();
+            }
+            else if (requester.Role.Name != Constants.PROJECT_ROLE_ADMIN)
+            {
+                throw new IncorrectRoleForbiddenRequestException();
+            }
+
+            _mapper.Map(tagForUpdateDto, tag);
+            await _repositoryManager.SaveAsync();
+        }
+
+        public async Task DeleteTagAsync(Guid tagId, bool trackChanges)
+        {
+            var requesterId = GetRequesterId();
+
+            var tag = await _repositoryManager.TagRepository.GetTagByIdAsync(tagId, trackChanges);
+
+            if (tag == null)
+            {
+                throw new TagNotFoundException(tagId);
+            }
+
+            var requester = await _repositoryManager.ProjectMemberRepository.GetProjectMemberAsync(tag.ProjectId, requesterId, false);
+
+            if (requester is null)
+            {
+                throw new NotAProjectMemberForbiddenRequestException();
+            }
+            else if (requester.Role.Name != Constants.PROJECT_ROLE_ADMIN)
+            {
+                throw new IncorrectRoleForbiddenRequestException();
+            }
+
+            _repositoryManager.TagRepository.DeleteTag(tag);
+            await _repositoryManager.SaveAsync();
+        }
+
+        private string GetRequesterId()
+        {
+            var claimsIdentity = (ClaimsIdentity)_contextAccessor.HttpContext.User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            return claim!.Value;
         }
     }
 }
