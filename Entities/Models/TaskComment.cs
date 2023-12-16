4b3ce6376a5cf5cce91f6a7a4819436834b12eaa
Anand S <anandsatheeshk@gmail.com>
2023-12-16 14:41:25 +0530

    Comment entity added to db

---
+++ Entities/Models/TaskComment.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models
+{
+    public class TaskComment
+    {
+        [Key]
+        public Guid CommentId { get; set; }
+        [Required(ErrorMessage = "Comment cannot be empty.")]
+        [MaxLength(500, ErrorMessage = "Maximum length of comment is 500 characters")]
+        public string? Comment { get; set; }
+        public DateTime CreatedAt { get; set; }
+        [ForeignKey(nameof(ProjectTask))]
+        public Guid TaskId { get; set; }
+        public ProjectTask ProjectTask { get; set;}
+        [ForeignKey(nameof(User))]
+        public string? CommenterId { get; set; }
+        public User? Commenter { get; set; }
+    }
+}
---
+++ FSPWebAPI/Migrations/20231216090254_AddCommentsTable.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FSPWebAPI.Migrations
+{
+    public partial class AddCommentsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    CommentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Comment = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TaskId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CommenterId = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.CommentId);
+                    table.ForeignKey(
+                        name: "FK_Comments_AspNetUsers_CommenterId",
+                        column: x => x.CommenterId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Comments_Tasks_TaskId",
+                        column: x => x.TaskId,
+                        principalTable: "Tasks",
+                        principalColumn: "TaskId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_CommenterId",
+                table: "Comments",
+                column: "CommenterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_TaskId",
+                table: "Comments",
+                column: "TaskId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comments");
+        }
+    }
+}
