92db20de5d6699538b893a7f295ca1c499a0bf69
Anand S <anandsatheeshk@gmail.com>
2023-12-16 16:53:13 +0530

    TaskComment dto configuration

---
+++ Shared/DataTransferObjects/TaskCommentForCreationDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.DataTransferObjects
+{
+    public record TaskCommentForCreationDto
+    {
+        [Required(ErrorMessage = "Comment cannot be empty.")]
+        [MaxLength(500, ErrorMessage = "Maximum length of comment is 500 characters")]
+        public string? Comment { get; set; }
+        [Required(ErrorMessage = "Commenter id should not be empty")]
+        public string? CommenterId { get; set; }
+    }
+}
