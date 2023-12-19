00a22807d45c49374be4e971c0bb947a132bdda6
Anand S <anandsatheeshk@gmail.com>
2023-12-19 09:44:14 +0530

    Stats service registration

---
+++ Service/StatsService.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Contracts;
+using Microsoft.AspNetCore.Http;
+using Service.Contracts;
+
+namespace Service
+{
+    public class StatsService : IStatsService
+    {
+        private readonly IRepositoryManager _repositoryManager;
+        private readonly ILoggerManager _logger;
+        private readonly IMapper _mapper;
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public StatsService(IRepositoryManager repositoryManager, ILoggerManager logger, IMapper mapper, IHttpContextAccessor contextAccessor)
+        {
+            _repositoryManager = repositoryManager;
+            _logger = logger;
+            _mapper = mapper;
+            _contextAccessor = contextAccessor;
+        }
+    }
+}
