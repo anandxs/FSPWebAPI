31a9f6a60ecf263e8f90e9ab23ae2f51b3666df1
Anand S <anandsatheeshk@gmail.com>
2023-12-26 21:47:21 +0530

    Update attachment entity

--- Entities/Models/Attachment.cs
+++ Entities/Models/Attachment.cs
@@ -10,9 +10,6 @@ namespace Entities.Models
         [Required(ErrorMessage = "File name is a required field.")]
         [MaxLength(256, ErrorMessage = "Maximum length of file name is 256 characters.")]
         public string? FileName { get; set; }
-        [Required(ErrorMessage = "File URL is a required field.")]
-        [MaxLength(2000, ErrorMessage = "Maximum length of file url is 1000 characters.")]
-        public string? FileUrl { get; set; }
         public DateTime CreatedAt { get; set; }
         [ForeignKey(nameof(ProjectTask))]
         public Guid TaskId { get; set; }
---
+++ FSPWebAPI/Migrations/20231226155659_RemoveFileUrlColumnFromAttachmentTable.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FSPWebAPI.Migrations
+{
+    public partial class RemoveFileUrlColumnFromAttachmentTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FileUrl",
+                table: "Attachments");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FileUrl",
+                table: "Attachments",
+                type: "nvarchar(2000)",
+                maxLength: 2000,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
