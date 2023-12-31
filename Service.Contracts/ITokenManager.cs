3c24da8d0983b504e078048fea98548f3d935f6b
Anand S <anandsatheeshk@gmail.com>
2023-12-31 20:04:13 +0530

    Service contract implicit usings

--- Service.Contracts/ITokenManager.cs
+++ Service.Contracts/ITokenManager.cs
@@ -2,12 +2,9 @@
 {
     public interface ITokenManager
     {
-        // blacklist user when blocked
         Task<bool> IsCurrentUserBlockedAsync();
         Task BlockUserAsync(string userId);
         Task UnBlockUserAsync(string userId);
-
-        // blacklist access token on logout
         Task<bool> IsCurrentActiveTokenAsync();
         Task DeactivateCurrentAsync();
         Task<bool> IsActiveAsync(string token);
